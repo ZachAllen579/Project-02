8cd72efcc93c4670f90cdd9a3ae3797364a834ea
Zach Allen <zachallen579@gmail.com>
2022-10-13 11:52:14 -0500

    pushing up the work ive done

---
+++ controllers/api/fbapi.js
@@ -0,0 +1,8 @@
+const options = {
+    method:'GET',
+    headers: {
+        'Ocp-Apim-Subscription-Key': `a908dae1-863a-40b3-a30d-c8f42983d49e`,
+        'languageId': 'English',
+        'Accept': 'application/xml'
+    }
+};
