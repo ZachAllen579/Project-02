60c2f3ecb13d5370d581017e534b21bfda01c802
Alex Luce <alexluce85@gmail.com>
2022-10-14 11:31:15 -0500

    small homeroutes changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,18 +15,14 @@ router.get('/', async (req, res) => {
     // });
 
     // Serialize data so the template can read it
-<<<<<<< HEAD
+
     //const projects = projectData.map((project) => project.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+     
       
-=======
-    // const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
     res.render('homepage', {  
->>>>>>> fc1da50f83a67fc171872430ee2147f04de72e2b
+
       logged_in: req.session.logged_in 
     });
   } catch (err) {
