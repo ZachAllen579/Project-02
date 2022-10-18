c784e51f6e9eba64e6d5cc8b09a7092938a16b25
Alex Luce <alexluce85@gmail.com>
2022-10-17 19:31:38 -0500

    post class work on team select page

---
+++ views/js/teamSelect.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("teamSelectBtn");
+
+function selectTeam(team) {
+    console.log(team);
+}
+
+
+btn.addEventListener('click', () => {
+    console.log ("")
+    selectTeam(team)
+  })
